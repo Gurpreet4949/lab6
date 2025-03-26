4d4e835d018c91367a5797c8a46dd74472634580
omercsx <omercagris@gmail.com>
2025-03-26 05:45:34 -0400

    connect backend

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const connectDB = require('./database/database');
+const analyticsRoutes = require('./routes/analyticsRoutes');
 const app = express();
 
 app.use(express.json());
 
 connectDB();
+
+app.use("/api/analytics", analyticsRoutes);
 
 app.listen(3000, () => {
   console.log('Server is running on port http://localhost:3000');
